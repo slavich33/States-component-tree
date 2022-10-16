38e70e5daf3f376b408da8435bc8f022ef01b0e7
Slava Chu <slavich33@icloud.com>
2022-10-16 13:16:07 +0300

    Ver2 Latest

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
-      <input onChange={props.change} type="text" value={props.text} />
+      <input onChange={handleChange} type="text" value={inputText} />
       <button
         onClick={() => {
-          props.button(props.text);
+          props.button(inputText);
+          setInputText("");
         }}
       >
         <span>Add</span>
